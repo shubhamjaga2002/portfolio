3f43686e5a880600fec0b755a3ed15c749544467
Shubham Jaga <shubhamjaga001@gmail.com>
2024-09-03 20:09:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+// HEADER ANIMATION START
+ScrollReveal().reveal(".header-text, .button-div, .photo-div",
+ { 
+ 	reset: true,
+ 	delay: 500,
+ 	distance: '80px',
+ 	duration: 1700,
+ 	interval: 300,
+ 	origin: 'top'
+ 	
+});
+
+// HEADER ANIMATION END
+
+// ABOUT ANIMATION START
+
+ScrollReveal().reveal(".about-img, .text-me",
+ { 
+ 	reset: true,
+ 	delay: 500,
+ 	distance: '80px',
+ 	duration: 1700,
+ 	interval: 300,
+ 	origin: 'top'	
+});
+
+// ABOUT ANIMATION END
+
+// SKILLS ANIMATION START
+
+ScrollReveal().reveal(".profesional, .my-goal, .skill-image, .html, .css, .bootstrap, .javascript",
+ { 
+ 	reset: true,
+ 	delay: 500,
+ 	distance: '80px',
+ 	duration: 1700,
+ 	interval: 300,
+ 	origin: 'top'	
+});
+
+// SKILLS ANIMATION END
+
+// WORK ANIMATION START
+
+ScrollReveal().reveal(".project-one, .project-two, .project-three, .project-four, .project-five, .project-six",
+ { 
+ 	reset: true,
+ 	delay: 500,
+ 	distance: '80px',
+ 	duration: 1700,
+ 	interval: 300,
+ 	origin: 'top'	
+});
+
+// WORK ANIMATION END
+
+// CONTACT ANIMATION START
+
+ScrollReveal().reveal(".calling, .map, .mail, .whatsapp",
+ { 
+ 	reset: true,
+ 	delay: 500,
+ 	distance: '50px',
+ 	duration: 1700,
+ 	interval: 300,
+ 	origin: 'top',
+ 	rotate: {
+        x: 10,
+        z: -10
+    } 
+
+});
+
+ScrollReveal().reveal(".name, .mobile, .email, .query, .button",
+ { 
+ 	reset: true,
+ 	delay: 500,
+ 	distance: '80px',
+ 	duration: 1700,
+ 	interval: 300,
+ 	origin: 'top',
+});
+
+// CONTACT ANIMATION END
+
+// FOOTER ANIMATION START
+
+ScrollReveal().reveal(".linkedin, .twitter, .facebook, .instagram, .youtube, .github",
+ { 
+       reset: true,
+       delay: 500,
+       distance: '80px',
+       duration: 1700,
+       interval: 300,
+       origin: 'top',
+       scale: 3
+});
+
+// FOOTER ANIMATION END
+
